dae6bca08ad6076d62ebd6b0d39a8bbb8ddaa05f
janli <janlin1805@gmail.com>
2025-10-08 20:23:15 +0200

    thymeleaf

---
+++ inventory-system/src/main/java/com/janlindauer/inventory_system/security/HomeController.java
@@ -0,0 +1,19 @@
+package com.janlindauer.inventory_system.security;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping({"/", "/index.html"})
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping({"/login", "/login.html"})
+    public String login() {
+        return "login";
+    }
+}
+
---
+++ inventory-system/src/main/java/com/janlindauer/inventory_system/security/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.janlindauer.inventory_system.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/error", "/index", "/login", "/css/**", "/js/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login.html")
+                        .loginProcessingUrl("/login")
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/logout.html?logout")
+                        .permitAll()
+                )
+                .build();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        UserDetails user = User
+                .withUsername("admin")
+                .password("{noop}password")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+}
